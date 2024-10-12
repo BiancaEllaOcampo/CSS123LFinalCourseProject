f4becd4027d3a1af10bd81f7e87f4a9f950671e0
BiancaEllaOcampo <bedocampo@mymail.mapua.edu.ph>
2024-10-12 21:46:41 +0800

    Employee&Department-Bianca-October12

--- Group4FinalCourseProject/src/main/java/com/group4/libraries/Employee.java
+++ Group4FinalCourseProject/src/main/java/com/group4/libraries/Employee.java
@@ -10,9 +10,61 @@ package com.group4.libraries;
  */
 abstract class Employee implements SalaryDetails {
     protected String name;
+    protected Department department;
+    /* protected Performance performance; */
+    protected String employeeDescription;
 
     public Employee(String name) {
         this.name = name;
     }
-    
+
+    // Constructor
+    public void Employee(Department department /** Performance performance **/
+    ) {
+        this.department = department;
+        /** this.performance = performance; **/
+        this.employeeDescription = ""; // Can be set by subclasses later
+    }
+
+    // Abstract method to be implemented by subclasses
+    public abstract void writeEmployeeDescription();
+
+    // Getter and setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and setter for employeeDescription
+    public String getEmployeeDescription() {
+        return employeeDescription;
+    }
+
+    public void setEmployeeDescription(String employeeDescription) {
+        this.employeeDescription = employeeDescription;
+    }
+
+    // Getter and setter for department
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    // Getter and setter for performance
+    /**
+     * public Performance getPerformance() {
+     * return performance;
+     * }
+     * 
+     * public void setPerformance(Performance performance) {
+     * this.performance = performance;
+     * }
+     **/
+
 }
